71e7f64abcb30167eae9e973c43304cef2fe3ced
DescartesILG <haydenmorgan618@gmail.com>
2025-11-18 21:08:07 -0600

    Create auxiliary.js

---
+++ data/auxiliary.js
@@ -0,0 +1,13 @@
+export const auxiliaryData = {
+  "FSV830": {
+    variants: {
+      "FSV830 - Standard": {
+        "Base Model": { hp: 8000, buffs: "" },
+        "Apex 5": { hp: "+4200hp", buffs: "+15% support" },
+        "Apex 10": { hp: "+8400hp", buffs: "+25% support" },
+        "Apex 15": { hp: "+12600hp", buffs: "+35% support" },
+        "Apex 20": { hp: "+16800hp", buffs: "+50% support" }
+      }
+    }
+  }
+};
