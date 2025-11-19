d3210cea09d3ded7f1302ebc73174acf7bbcab43
DescartesILG <haydenmorgan618@gmail.com>
2025-11-18 21:07:40 -0600

    Create battlecruiser.js

---
+++ data/battlecruiser.js
@@ -0,0 +1,13 @@
+export const battlecruiserData = {
+  "Spear of Uranus": {
+    variants: {
+      "Spear - Standard": {
+        "Base Model": { hp: 11000, buffs: "" },
+        "Apex 5": { hp: "+6500hp", buffs: "+12% kinetic dmg" },
+        "Apex 10": { hp: "+13000hp", buffs: "+20% kinetic dmg" },
+        "Apex 15": { hp: "+19500hp", buffs: "+28% kinetic dmg" },
+        "Apex 20": { hp: "+26000hp", buffs: "+36% kinetic dmg" }
+      }
+    }
+  }
+};
