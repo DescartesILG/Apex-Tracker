f64826f252629b505477700396509c97f4d204a4
DescartesILG <haydenmorgan618@gmail.com>
2025-11-18 21:06:29 -0600

    Create battleship.js

---
+++ data/battleship.js
@@ -0,0 +1,13 @@
+export const battleshipData = {
+  "Warspite": {
+    variants: {
+      "Warspite - Standard": {
+        "Base Model": { hp: 15000, buffs: "" },
+        "Apex 5": { hp: "+8000hp", buffs: "+20% armor" },
+        "Apex 10": { hp: "+16000hp", buffs: "+30% armor" },
+        "Apex 15": { hp: "+24000hp", buffs: "+40% armor" },
+        "Apex 20": { hp: "+32000hp", buffs: "+55% armor" }
+      }
+    }
+  }
+};
