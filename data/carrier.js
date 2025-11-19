a0a632c9db7f59d41293a09eaaa673be1ce1b8f6
DescartesILG <haydenmorgan618@gmail.com>
2025-11-18 21:07:18 -0600

    Create carrier.js

---
+++ data/carrier.js
@@ -0,0 +1,14 @@
+export const carrierData = {
+  "Solar Whale": {
+    variants: {
+      "Solar Whale - Standard": {
+        "Base Model": { hp: 12000, buffs: "" },
+        "Apex 5": { hp: "+7000hp", buffs: "+1 fighter bay" },
+        "Apex 10": { hp: "+14000hp", buffs: "+2 fighter bay" },
+        "Apex 15": { hp: "+21000hp", buffs: "+3 fighter bay" },
+        "Apex 20": { hp: "+28000hp", buffs: "+4 fighter bay" }
+      }
+    }
+  }
+};
+// add other carriers similarly
